69acd6ae5f2e86b4f6ac09dd9e99619abd91f983
tt <xiaotian3303@outlook.com>
2026-02-13 22:20:29 +0800

    search 布局

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,19 @@ function Home() {
     { id: 3, title: "John 3", release_date: "2023" },
   ];
 
+  const handleSearch = () => {};
   return (
     <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
       <div className="movies-grid">
         {movies.map((movie) => (
           <MovieCard movie={movie} key={movie.id} />
