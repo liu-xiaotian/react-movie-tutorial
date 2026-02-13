2ce73048c7b3ac9a84f6966a7fcdd33cead2518b
tt <xiaotian3303@outlook.com>
2026-02-13 22:56:35 +0800

    router

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## 创建项目
 
-~~~
+```
 npm create vite@latest
-~~~
-
+npm install react-router-dom
+```
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,12 +27,9 @@ function Home() {
         </button>
       </form>
       <div className="movies-grid">
-        {movies.map(
-          (movie) =>
-            movie.title.toLowerCase().startsWith(searchQuery) && (
-              <MovieCard movie={movie} key={movie.id} />
-            ),
-        )}
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
       </div>
     </div>
   );
