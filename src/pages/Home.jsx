90fc34f463e4f014da29032becb02b4700cc696e
tt <xiaotian3303@outlook.com>
2026-02-13 22:41:44 +0800

    更新状态,阻止刷新，简单搜索

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,17 @@
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
   const movies = [
     { id: 1, title: "John 1", release_date: "2020" },
     { id: 2, title: "John 2", release_date: "2022" },
     { id: 3, title: "John 3", release_date: "2023" },
   ];
 
-  const handleSearch = () => {};
+  const handleSearch = (e) => {
+    e.preventDefault(); //阻止默认行为(页面刷新)
+    alert(searchQuery);
+  };
   return (
     <div className="home">
       <form onSubmit={handleSearch} className="search-form">
@@ -14,15 +19,20 @@ function Home() {
           type="text"
           placeholder="Search for movies..."
           className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
         <button type="submit" className="search-button">
           Search
         </button>
       </form>
       <div className="movies-grid">
-        {movies.map((movie) => (
-          <MovieCard movie={movie} key={movie.id} />
-        ))}
+        {movies.map(
+          (movie) =>
+            movie.title.toLowerCase().startsWith(searchQuery) && (
+              <MovieCard movie={movie} key={movie.id} />
+            ),
+        )}
       </div>
     </div>
   );
