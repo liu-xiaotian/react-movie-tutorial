ffd40e91db56476ba1ad602c41dbd07d243cf301
tt <xiaotian3303@outlook.com>
2026-02-13 23:33:50 +0800

    api

---
+++ src/services/api.js
@@ -0,0 +1,16 @@
+const API_KEY = "4a6bf363adefc23d3b3f2a28ff1e4c61";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/movie/popular?api_key=${API_KEY}&query=${encodeURIComponent(query)}`,
+  );
+  const data = await response.json();
+  return data.results;
+};
